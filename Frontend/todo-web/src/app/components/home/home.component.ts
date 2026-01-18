0f856fa60cb99b9957daa8c2435e1c16e91aca30
Igor Rafael <igorcoelho808@gmail.com>
2026-01-17 21:21:51 -0300

    Feat: Estrutura dos cartões estabelecida.

--- Frontend/todo-web/src/app/components/home/home.component.ts
+++ Frontend/todo-web/src/app/components/home/home.component.ts
@@ -1,11 +1,24 @@
+import { NgIf, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgOptimizedImage, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+   description = false;
+
+  toggleDescription(){
+    this.description = !this.description;
+  }
+
+  if (description = true) {
+
+
+
+  }
 
 }
