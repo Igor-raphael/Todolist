b0ddbc1c225ab5390bf53d41066f52d5f1530197
Igor Rafael <igorcoelho808@gmail.com>
2026-01-19 22:05:47 -0300

    Feat: Componente Modal.

--- Frontend/todo-web/src/app/components/home/home.component.ts
+++ Frontend/todo-web/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { NgIf, NgOptimizedImage, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NgOptimizedImage, NgIf, NgClass],
+  imports: [NgOptimizedImage, NgIf, NgClass, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
