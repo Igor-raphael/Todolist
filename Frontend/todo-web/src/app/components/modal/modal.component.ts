6b4c71afbfc06a14b366713570af4f3616a0da22
Igor Rafael <igorcoelho808@gmail.com>
2026-01-22 21:09:20 -0300

    Revert "Feat: Função para liberação do btn."
    This reverts commit 5145d21f6e027260b4ea227200a433c8de395528.

--- Frontend/todo-web/src/app/components/modal/modal.component.ts
+++ Frontend/todo-web/src/app/components/modal/modal.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-modal',
-  imports: [NgIf, FormsModule],
+  imports: [NgIf],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
@@ -14,22 +12,9 @@ export class ModalComponent {
   showForm: boolean = false;
   overlay: boolean = false;
 
-  text: string = '';
-
   formToggle(){
     this.showForm = !this.showForm;
     this.overlay = !this.overlay;
-  }
-
-  isLiberado(): boolean{
-
-    let liberado : boolean = false;
-
-    if(this.text == ''){
-          return liberado = true;
-    }
-
-    return liberado;
   }
 
 }
