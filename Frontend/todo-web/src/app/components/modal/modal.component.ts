74d16910196bcb8ae2de60fbe17866592204724b
Igor Rafael <igorcoelho808@gmail.com>
2026-01-22 21:29:10 -0300

    Feat: Implementada função que controla o funcionamento do btn(form).

--- Frontend/todo-web/src/app/components/modal/modal.component.ts
+++ Frontend/todo-web/src/app/components/modal/modal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-modal',
-  imports: [NgIf],
+  imports: [NgIf, FormsModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
@@ -12,9 +14,17 @@ export class ModalComponent {
   showForm: boolean = false;
   overlay: boolean = false;
 
+  text: string = '';
+
   formToggle(){
     this.showForm = !this.showForm;
     this.overlay = !this.overlay;
+  }
+
+  isLiberado(): boolean{
+
+   return this.text.trim().length > 0;
+
   }
 
 }
