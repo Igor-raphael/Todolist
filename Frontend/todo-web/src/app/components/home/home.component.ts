ca3d1857cc8a704e55620f295d327cadda4c2f9d
Igor Rafael <igorcoelho808@gmail.com>
2026-01-23 21:37:11 -0300

    Test: Funcionamento via test.json, confirmado.

--- Frontend/todo-web/src/app/components/home/home.component.ts
+++ Frontend/todo-web/src/app/components/home/home.component.ts
@@ -1,14 +1,27 @@
-import { NgIf, NgOptimizedImage, NgClass } from '@angular/common';
+import { NgOptimizedImage, CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
+import { ListService } from '../../services/list-service';
+import { Observable } from 'rxjs';
+import { Tarefas } from '../../model/tarefas';
 
 @Component({
   selector: 'app-home',
-  imports: [NgOptimizedImage, NgIf, NgClass, ModalComponent],
+  standalone: true,
+  imports: [NgOptimizedImage, ModalComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  tarefas$: Observable<Tarefas[]>;
+
+  constructor( private listService: ListService){
+
+    this.tarefas$ = this.listService.list();
+
+  }
+
 
    description: boolean = false;
    done: boolean = false;
--- Frontend/todo-web/src/app/services/list-service.ts
+++ Frontend/todo-web/src/app/services/list-service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Tarefas } from '../model/tarefas';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ListServiceService {
+export class ListService {
 
   //private readonly API = 'http://localhost:8080/todos';
 
-  private readonly API = 'assets/test.json';
+  private readonly API = '/assets/test.json';
 
   constructor(private httpClient: HttpClient) {}
 
-list(){
+list(): Observable<Tarefas[]>{
     return this.httpClient.get<Tarefas[]>(this.API);
   }
 
