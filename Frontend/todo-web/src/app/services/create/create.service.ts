e579ff91f5e70ec7f756308b3c77d20edfbfde2a
Igor Rafael <igorcoelho808@gmail.com>
2026-01-28 23:52:08 -0300

    Fix: Ajustando o comportamento da descrição.

--- Frontend/todo-web/src/app/services/create/create.service.ts
+++ Frontend/todo-web/src/app/services/create/create.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Tarefas } from '../../model/tarefas';
 import { TarefaDTO } from '../../model/TarefaDTO';
 
 @Injectable({
